10d4708fd37ea47342c6feda4521b8d7b38a45c2
Simon Stiefel <mail@simonboots.com>
2019-11-04 20:24:13 +1000

    Initial Commit

---
+++ SwiftUIFourierSeries/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFourierSeries
+//
+//  Created by Simon Stiefel on 4/11/19.
+//  Copyright © 2019 Simon Stiefel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

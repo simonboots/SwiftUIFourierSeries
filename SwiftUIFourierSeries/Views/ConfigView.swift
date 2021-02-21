2a5ac4de58154b3ff7ac27718a6bca14657527e5
Jess <jtrinh619@gmail.com>
2021-02-21 11:00:01 -0800

    cleaned up code for handling all screen sizes and orientations

--- SwiftUIFourierSeries/Views/ConfigView.swift
+++ SwiftUIFourierSeries/Views/ConfigView.swift
@@ -8,47 +8,28 @@ struct ConfigView: View {
     
     var body: some View {
         // https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/adaptivity-and-layout/
-        Group {
             if verticalSizeClass == .regular && horizontalSizeClass == .compact {
                 // iPhone Portrait or iPad 1/3 split view for Multitasking for instance
                 // A
                 // B
                 // C
                 VStack {
-                    Spacer()
                     VStack {
                         ButtonsView(store: store).padding()
-                        Spacer()
                         PickerView(store: store).padding()
                     }
-                    Spacer()
                 }
-            } else if verticalSizeClass == .compact && horizontalSizeClass == .compact {
-                // some "standard" iPhone Landscape (iPhone SE, X, XS, 7, 8, ...)
+            } else {
+                // Landscape layout for all other size class combinations
                 // A B C
                 VStack {
-                    Spacer()
                     HStack {
                         ButtonsView(store: store).padding()
-                        Spacer()
                         PickerView(store: store).padding()
                     }
-                    Spacer()
-                }
-            } else if verticalSizeClass == .compact && horizontalSizeClass == .regular {
-                // some "bigger" iPhone Landscape (iPhone Xs Max, 6s Plus, 7 Plus, 8 Plus, ...)
-                // A B C D
-                VStack {
-                    Spacer()
-                    HStack {
-                        ButtonsView(store: store)
-                        Spacer()
-                        PickerView(store: store)
-                    }
-                    Spacer()
                 }
             }
-        }
+        
     }
 }
 
@@ -57,6 +38,14 @@ struct ConfigView_Previews: PreviewProvider {
         let timer = Timer(frequency: 0.2)
         let store = Store(kind: .squareWave, numOfFunctions: 10, timer: timer)
         
-        return ConfigView(store: store)
+        return Group {
+            ConfigView(store: store)
+            ConfigView(store: store)
+                .previewDevice("iPhone 12 Pro Max")
+                .previewLayout(.device)
+            ConfigView(store: store)
+                .previewDevice("iPhone 12 Pro Max")
+                .previewLayout(.device)
+        }
     }
 }
